1c69cb89e2494ecc3c7d4240ebf53750962891ca
phbelucci <phbelucci@gmail.com>
2020-08-31 22:17:48 -0300

    react projeto

---
+++ eletroposto-web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import api from '../src/api/index'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Api>
+      </Api>
+    </div>
+  );
+}
+
+export default App;
---
+++ eletroposto-web/src/api/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Api() {
+  return (
+    <div>ok</div>
+  );
+}
+
+export default Api;
